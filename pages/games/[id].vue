2bfc3a9d69e4b507ed8eb4f97a1f7a1c044ba294
NikSleepy <nikriaria@gmail.com>
2024-08-11 23:03:08 +0700

    update: json input

--- pages/games/[id].vue
+++ pages/games/[id].vue
@@ -1,44 +1,56 @@
 <template>
-  <div class="text-white text-3xl w-full ">
-    <p>bang</p>
-    <p>{{ item.name }}</p>
-    <p>{{ index }}</p>
+  <div class="text-white text-3xl w-full">
+    <div v-for="label in item.orders">
+      <div class="flex gap-4">
+        <p>{{ label.no }}</p>
+        <p>{{ label.label }}</p>
+      </div>
+
+      <div v-for="input in label.inputs">
+        <p>{{ label.type }}</p>
+        <input
+          v-if="label.type !== 'select'"
+          :type="label.type"
+          :id="input.id"
+          v-model="formData[input.id]"
+          :required="input.required"
+          :placeholder="input.placeholder"
+        />
+
+        <select
+          v-else
+          :name="input.category"
+          :id="input.id"
+          v-model="formData[input.id]"
+        >
+          <option
+            v-for="option in input.category"
+            :key="option"
+            :value="option"
+          >
+            <p>{{ option.name }}</p>
+          </option>
+        </select>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-const data = [
-  {
-    name: 'PUBG',
-  },
-  {
-    name: 'Genshin Impact',
-  },
-  {
-    name: 'Mobile Legends',
-  },
-  {
-    name: 'Honkai Star Rail',
-  },
-  {
-    name: 'Call Of Duty',
-  },
-  {
-    name: 'Free Fire',
-  },
-];
+import data from '@/mocks/inputTopup.json';
 
 export default {
   data() {
     return {
       item: {},
       index: null,
+      formData: {},
     };
   },
   mounted() {
     const id = parseInt(this.$route.params.id);
     this.item = data[id - 1];
     this.index = id;
-  }
+  },
 };
 </script>
